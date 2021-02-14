1f3f29f295948de0030cd96b06c4a3aef0c9a655
joydelynbaes <joydeyn@gmail.com>
2021-02-14 15:49:16 +0700

    lesson6

--- lesson_6/js/windchill.js
+++ lesson_6/js/windchill.js
@@ -1,10 +1,16 @@
+function doInputOutput(){
+    let temp = parseFloat(document.getElementById("temp").value);
+    let speed = parseFloat(document.getElementById("wind").value);
 
+    let result = windChill(temp, speed);
+    document.getElementById("outPut").innerHTML = result;
+} 
+function windChill(temp, speed){
+    let t = temp;
+    let s = speed;
+    let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
+    return chillFac;
+}
 
-
-		let temp = 49;//parseFloat(document.getElementById("temp").value);
-		let speed = 12; //parseFloat(document.getElementById("wind").value);
-
-		let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
-		document.getElementById("chillOutput").innerHTML = chill.toFixed(2);
-
-	}
+result = 35.74 + 0.6215 * 45 - 35.75 * Math.pow(4.0, 0.16) + 0.4275 * 45 * Math.pow(4.0, 0.16);
+document.getElementById("chillOutput").textContent = Math.round(result);
