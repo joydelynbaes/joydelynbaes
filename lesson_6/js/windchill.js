34bdb8bb9a70122b5861191105b8796a29f38f39
joydelynbaes <76957652+joydelynbaes@users.noreply.github.com>
2021-02-15 11:42:15 +0800

    Update windchill.js

--- lesson_6/js/windchill.js
+++ lesson_6/js/windchill.js
@@ -11,6 +11,3 @@ function windChill(temp, speed){
     let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
     return chillFac;
 }
-
-result = 35.74 + 0.6215 * 45 - 35.75 * Math.pow(4.0, 0.16) + 0.4275 * 45 * Math.pow(4.0, 0.16);
-document.getElementById("chillOutput").textContent = Math.round(result);
