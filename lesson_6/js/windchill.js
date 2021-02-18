d3d7d4606b3902ea77e007347a04d13b846d731f
joydelynbaes <joydeyn@gmail.com>
2021-02-18 09:57:53 +0700

    up

--- lesson_6/js/windchill.js
+++ lesson_6/js/windchill.js
@@ -1,6 +1,6 @@
 function doInputOutput(){
-    let temp = parseFloat(document.getElementById("temp").value);
-    let speed = parseFloat(document.getElementById("wind").value);
+    let temp = 49;
+    let speed = 12;
 
     let result = windChill(temp, speed);
     document.getElementById("chillOutput").innerHTML = result;
@@ -8,7 +8,16 @@ function doInputOutput(){
 function windChill(temp, speed){
     let t = temp;
     let s = speed;
-    let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
-    return chill;
+    let chillF;
+
+    if (t > 50 && s < 4.8){
+        chillF = "N/A";
+        return chillF;
+    }
+    else{
+        chillF = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
+        return chillF.toFixed(0) + "&#176;F";
+    }
+
 }
 doInputOutput()
