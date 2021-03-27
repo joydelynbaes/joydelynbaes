290c459199b991fce31aa79cd0630766e5997070
joydelynbaes <joydeyn@gmail.com>
2021-03-27 23:12:28 +0700

    update

---
+++ lesson_11/js/app.js
@@ -0,0 +1,28 @@
+let imagesToLoad = document.querySelectorAll ("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute ("src", image.getAttribute("data-src"));
+    image.onload = () => {
+    image.removeAttribute("data-src");
+    };
+};
+
+if("intersectionObserver" in window){
+    const observer = new intersectionObserver ((items, observer) =>{
+        items.forEach ((item) => {
+            if (item.isIntersecting){
+                loadImages(item.target);
+                observer.unobserver(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+        imagesToLoad.forEach((img) => {
+            loadImages(img);
+        });
+    }
+
+
